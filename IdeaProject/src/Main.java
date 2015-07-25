a7e3fa3d1499db5726fc3f4aa56a377335b51e92
unknown <smamran09@gmail.com>
2015-07-25 17:53:09 +0600

    dummy message

--- IdeaProject/src/Main.java
+++ IdeaProject/src/Main.java
@@ -1,8 +1,55 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+
+class MainWindow extends JFrame {
+    MainWindow() {
+        super("This is Main window");
+
+        JLabel label = new JLabel("Hello");
+        JPanel panel = new JPanel();
+        panel.add(label);
+        MainWindow mw = new MainWindow();
+        mw.add(panel);
+
+
+        /*
+            For Exiting with escape key
+        */
+
+        getRootPane().getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(
+                KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), "Cancel"
+        );
+        getRootPane().getActionMap().put("Cancel", new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+
+    }
+}
+
+class District {
+    private static String name;
+
+    public void setDistrict(String distName) {
+        this.name = distName;
+    }
+
+    public String getDist() {
+        return name;
+    }
+}
+
+
 public class Main {
     public static void main(String[] args) {
-        for (int i = 0; i < 10; i++) {
-            System.out.println(i*i);
-        }
-        System.out.println("Hello World!");
+        District dist = new District();
+        dist.setDistrict("Dhaka");
+        MainWindow mw = new MainWindow();
+        mw.setVisible(true);
+        mw.setSize(400, 400);
+        mw.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
