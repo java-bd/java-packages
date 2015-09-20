2732e54576c3d98da22f7f2195cb11bf2572861b
s.m.amran <smamran09@gmail.com>
2015-09-20 19:59:00 +0600

    Update MandelbrotExitByEscKey.java
    indented code

--- SWING/MandelbrotExitByEscKey.java
+++ SWING/MandelbrotExitByEscKey.java
@@ -37,15 +37,15 @@ public class MandelbrotExitByEscKey extends JFrame {
         }
 		
 		
-		getRootPane().getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(
-        KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), "Cancel");
-        getRootPane().getActionMap().put("Cancel", new AbstractAction()
-        { 
-
-            public void actionPerformed(ActionEvent e)
-            {
-                System.exit(0);
-            }
+	getRootPane().getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(
+	        KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), "Cancel");
+	        getRootPane().getActionMap().put("Cancel", new AbstractAction()
+	        { 
+	
+	            public void actionPerformed(ActionEvent e)
+	            {
+	                System.exit(0);
+	            }
        });
 		
     }
